cc6e1329d0a2d3e43af7933cb7238b22480c2f4e
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-09-09 13:54:08 -0500

    server set up

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+//const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+app.set('port', port)
+
+
+
+//Routes
+app.get('/', (req, res) => {
+	res.send('<h1>Hello hello</h1>')
+})
+
+app.listen(port, () => {
+	console.log(`Listening on port: ${port}`)
+})
+
